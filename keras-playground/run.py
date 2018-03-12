ceb96253f4c97529f0ac19013a560edb7088d9bc
Dan Cohen <dan.cohen@codurance.com>
2018-03-12 21:14:08 +0000

    Rename directories and continue playing with keras

--- keras-playground/run.py
+++ keras-playground/run.py
@@ -4,15 +4,16 @@
 import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
-
+from keras import optimizers
 
 model = Sequential()
-model.add(Dense(1, activation='linear', input_shape=(1,)))
+model.add(Dense(1, activation='tanh', input_shape=(1,)))
 
 model.summary()
 
+rmsProp = optimizers.RMSprop(lr=0.01, decay=1e-6)
 model.compile(loss='mse',
-              optimizer='adam',
+              optimizer=rmsProp,
               metrics=['accuracy'])
 
 x_train = np.array(range(1, 10000, 1))
@@ -22,5 +23,6 @@ y_test = np.array(range(6, 600, 6))
 
 model.fit(x_train, y_train, epochs=100)
 
-print "Evaluation"
-print model.evaluate(x_test, y_test)
+print("Evaluation")
+print (model.evaluate(x_test, y_test))
+model.summary()
